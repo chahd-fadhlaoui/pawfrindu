30e466bc52e9010de456815a48ba0db9e42d423b
CBG003 <cherifabenghorbel@gmail.com>
2025-03-27 23:51:38 +0100

    fixed the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 // App.jsx
-import "leaflet/dist/leaflet.css";
 import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import ResetPasswordConfirmation from './components/auth/ResetPasswordConfirmation';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import AdoptionRequestsTab from './components/PetManagement/AdoptionRequestsTab';
-import PetPostsTab from './components/PetManagement/PetPostsTab';
 import ProtectedRoute from './components/ProtectedRoute';
 import PublicRoute from './components/PublicRoute';
 import AppContextProvider, { useApp } from './context/AppContext';
@@ -23,26 +20,12 @@ import PetManagementDashboard from './pages/PetManagementDashboard';
 import Pets from './pages/Pets';
 import Profile from './pages/Profile';
 import TrainerDashboard from './pages/Trainer/TrainerDashboard';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import  VetDashboard from './pages/VetDashboard/VetDashboard';
 import Veteriniandetail from './pages/Veteriniandetail';
 import Veterinarians from './pages/VetAppointment/Veterinarians';
 import VetDetails from './pages/VetAppointment/VetDetails';
 import "leaflet/dist/leaflet.css";
 import VetPendingApproval from './pages/VetDashboard/VetPendingApproval';
-=======
-import { VetDashboard } from './pages/Vet/VetDashboard';
-import VetDetails from './pages/VetAppointment/VetDetails';
-import Veterinarians from './pages/VetAppointment/Veterinarians';
-import Veteriniandetail from './pages/Veteriniandetail';
->>>>>>> Stashed changes
-=======
-import { VetDashboard } from './pages/Vet/VetDashboard';
-import VetDetails from './pages/VetAppointment/VetDetails';
-import Veterinarians from './pages/VetAppointment/Veterinarians';
-import Veteriniandetail from './pages/Veteriniandetail';
->>>>>>> Stashed changes
 
 const Layout = ({ children }) => {
   const location = useLocation();
@@ -98,13 +81,7 @@ const AppRoutes = () => {
       <Route path="/vet" element={<ProtectedRoute allowedRoles={["Vet"]}><VetDashboard /></ProtectedRoute>} />
       <Route path="/vet-pending-approval" element={<ProtectedRoute allowedRoles={["Vet"]}><VetPendingApproval/></ProtectedRoute>} /> {/* Add this route */}
       <Route path="/addPet" element={<ProtectedRoute allowedRoles={["PetOwner"]}><CreatePet /></ProtectedRoute>} />
-      <Route path="/pet-listing" element={<ProtectedRoute allowedRoles={["PetOwner"]}><PetPostsTab /></ProtectedRoute>} />
-      <Route path="/adoption-requests" element={<ProtectedRoute allowedRoles={["PetOwner"]}><AdoptionRequestsTab /></ProtectedRoute>} />
-<<<<<<< Updated upstream
       <Route path="/list/*" element={<ProtectedRoute allowedRoles={["PetOwner"]}><PetManagementDashboard /></ProtectedRoute>} />
-=======
-      <Route path="/list" element={<ProtectedRoute allowedRoles={["PetOwner"]}><PetManagementDashboard /></ProtectedRoute>} />
->>>>>>> Stashed changes
       <Route path="/profile" element={<ProtectedRoute allowedRoles={["PetOwner", "Trainer", "Vet"]}><Profile /></ProtectedRoute>} />
       <Route path="/myprofile" element={<ProtectedRoute allowedRoles={["PetOwner", "Trainer", "Vet"]}><CreateProfile /></ProtectedRoute>} />
       <Route path="/candidates/:petId" element={<ProtectedRoute allowedRoles={["PetOwner", "Trainer", "Vet"]}><CandidatesPage /></ProtectedRoute>} />
